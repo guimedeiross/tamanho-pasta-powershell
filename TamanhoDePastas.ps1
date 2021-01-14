fd185fd23e22ad4a712e54eab40b0822fb183b25
Guilherme Schubert Medeiros <guilhermeschu10@gmail.com>
2021-01-14 12:12:19 -0300

    Versão inicial script

---
+++ TamanhoDePastas.ps1
@@ -0,0 +1,27 @@
+﻿##para executar no powersehll navegue até a pasta onde está o script e execute da seguinte forma . .\TamanhoDePastas.ps1
+##e após isso chame a função Get-FolderSize -Folders Folder1,Folder2,Folder3
+
+
+Function Get-FolderSize {
+    param(
+    [Parameter(Mandatory=$true)][string[]]$Folders
+    )
+    
+    Begin{
+        Foreach($Folder in $Folders){
+            if(!(Get-item $Folder)){
+                Throw "Erro, $FOLDER não existe ou você não possui permissão nessa pasta. Favor especificar o caminho completo da pasta." 
+            }
+        }
+    }
+    Process{
+        Foreach($Folder in $Folders){
+            $Size = 0
+            Get-ChildItem $Folder -Recurse -Attributes "Hidden,!Directory" -ErrorAction silentlycontinue | %{$Size+=$_.Length} 
+            [pscustomobject]@{Folder=$Folder;Size="{0:N2}" -f($SIZE/1GB)+" GB"}    
+        }
+    }
+    End{
+        
+    }
+}
